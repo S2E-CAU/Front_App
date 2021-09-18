4e8d72fef1e88c51a766f611beaafa4b5dcc0fb7
seoha <seohayn127@gmail.com>
2021-09-18 20:50:50 +0900

    first commit

---
+++ app/src/main/java/com/example/solarcommunity/DjangoAPI.java
@@ -0,0 +1,17 @@
+package com.example.solarcommunity;
+
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface DjangoAPI {
+    //    String DJANGO_SITE="http://127.0.0.1:8000";
+    String DJANGO_SITE="http://10.0.2.2:8000";
+
+    @Multipart
+    @POST("api/map")
+    Call <ResponseBody> uploadFile(@Part MultipartBody.Part file);
+}
